3c0373e116bd94abea8f27b09a5282c9e0e3db47
zhao lu <zlu@zlus-MBP.local>
2009-12-15 01:09:15 -0800

    disable forgery protection for facebooker

--- config/environment.rb
+++ config/environment.rb
@@ -10,7 +10,8 @@ Rails::Initializer.run do |config|
   # Settings in config/environments/* take precedence over those specified here.
   # Application configuration should go into files in config/initializers
   # -- all .rb files in that directory are automatically loaded.
-
+  config.action_controller.allow_forgery_protection = false
+  
   # Add additional load paths for your own custom dirs
   # config.load_paths += %W( #{RAILS_ROOT}/extras )
 
