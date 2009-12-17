f3199b9875e2c5a071dbd68640f9b59ce1f57c1a
zhao lu <zlu@zlus-MBP.local>
2009-12-17 14:59:11 -0800

    add users to routes

--- config/routes.rb
+++ config/routes.rb
@@ -34,6 +34,7 @@ ActionController::Routing::Routes.draw do |map|
   # map.root :controller => "welcome"
   map.resources :friends
   map.root :controller=>"friends", :action=>"index"
+  map.resources :users
   
   # See how all your routes lay out with "rake routes"
 
