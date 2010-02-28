8f8d503b449a8cad9289ece220e1e6811a4ef5f8
Zhao Lu <zlu@zlu-imac.local>
2010-02-27 20:54:53 -0800

    disable exceptional

--- config/environment.rb
+++ config/environment.rb
@@ -21,7 +21,7 @@ Rails::Initializer.run do |config|
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
   config.gem "facebooker"
-  config.gem 'exceptional', :version => '2.0.10'
+#  config.gem 'exceptional', :version => '2.0.10'
   
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
