ad9101b5daae9d55c5b512087c65cac56ac64f01
Zhao Lu <zlu@zlu-imac.local>
2010-03-31 00:07:33 -0700

    correct the number user display

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def index
-    all_user_fb_ids = User.all.map(&:fb_id).sort
-    @user_fb_ids = all_user_fb_ids.paginate({ :page => params[:page], :per_page => 10 })
+    @all_user_fb_ids = User.all.map(&:fb_id).sort
+    @user_fb_ids = @all_user_fb_ids.paginate({ :page => params[:page], :per_page => 10 })
   end
 end
