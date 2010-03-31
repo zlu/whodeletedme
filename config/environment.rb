4a8dfb14866c528707f8097d7e807ee54216a593
Zhao Lu <zlu@zlu-imac.local>
2010-03-31 00:05:20 -0700

    include will_paginate gem

--- config/environment.rb
+++ config/environment.rb
@@ -21,9 +21,9 @@ Rails::Initializer.run do |config|
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
   config.gem "facebooker"
-  require 'will_paginate'
-  
-#  config.gem 'exceptional', :version => '2.0.10'
+  config.gem 'will_paginate'
+ 
+  # config.gem 'exceptional', :version => '2.0.10'
   
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
