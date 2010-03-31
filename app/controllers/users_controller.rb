7c5a78bdd4b7f646cf2f334406271aa486d8119c
Zhao Lu <zlu@zlu-imac.local>
2010-03-31 00:00:17 -0700

    correct the param problem for calling paginate on array

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def index
     all_user_fb_ids = User.all.map(&:fb_id).sort
-    @user_fb_ids = all_user_fb_ids.paginate(:all, :page => params[:page], :per_page => 10)
+    @user_fb_ids = all_user_fb_ids.paginate({ :page => params[:page], :per_page => 10 })
   end
 end
