8d15c6a054c2e8943f9ecac40bb8311e02fe9515
Zhao Lu <zlu@zlu-imac.local>
2010-03-31 00:34:51 -0700

    changing update target

--- lib/remote_link_renderer.rb
+++ lib/remote_link_renderer.rb
@@ -1,3 +1,4 @@
+
 class RemoteLinkRenderer < WillPaginate::LinkRenderer
   def prepare(collection, options, template)
     @remote = options.delete(:remote) || {}
@@ -6,6 +7,6 @@ class RemoteLinkRenderer < WillPaginate::LinkRenderer
 
 protected
   def page_link(page, text, attributes = {})
-    @template.link_to_remote(text, {:url => url_for(page), :method => :get}.merge(@remote))
+    @template.link_to_remote(text, { :url => url_for(page), :method => :get }.merge(@remote))
   end
 end
