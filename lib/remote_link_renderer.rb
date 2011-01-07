92c4554dd0cb527f72ff87967f2d93acd35dbceb
zhao lu <zlu@me.com>
2011-01-07 02:58:05 -0800

    change remote link render to post

--- lib/remote_link_renderer.rb
+++ lib/remote_link_renderer.rb
@@ -7,6 +7,6 @@ class RemoteLinkRenderer < WillPaginate::LinkRenderer
 
 protected
   def page_link(page, text, attributes = {})
-    @template.link_to_remote(text, { :url => url_for(page), :method => :get }.merge(@remote))
+    @template.link_to_remote(text, { :url => url_for(page), :method => :post }.merge(@remote))
   end
 end
